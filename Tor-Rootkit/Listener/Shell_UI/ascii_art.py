eba948ddd5febab790773168edd5ab81042b70d4
Nirjhar Biswas <75828293+Nirj2004@users.noreply.github.com>
2022-03-04 00:07:27 +0530

    Update ascii_art.py

--- Tor-Rootkit/Listener/Shell_UI/ascii_art.py
+++ Tor-Rootkit/Listener/Shell_UI/ascii_art.py
@@ -1,6 +1,18 @@
 from colorama import init, Fore, Style
 def draw():
     init()
-    print('TORROOTKIT')
+    print('T T T T T T T T T T T T T             O O O O O O O O              R R R R R R R R')
+    print('T T T T T T T T T T T T T             O O O O O O O O              R R R R R R R R')
+    print('        T T T                         O O O O O O O O              R R            R R')
+    print('        T T T                         O O O O O O O O              R R              R R')
+    print('        T T T                         O O O O O O O O              R R                R R')
+    print('        T T T                         O O O O O O O O              R R R R R R R R R R R R')
+    print('        T T T                         O O O O O O O O              R R R R R R R R R R R R')
+    print('        T T T                         O O O O O O O O              R R R R')
+    print('        T T T                         O O O O O O O O              R R    R R')
+    print('        T T T                         O O O O O O O O              R R      R R')
+    print('        T T T                         O O O O O O O O              R R         R R')
+    print('        T T T                         O O O O O O O O              R R           R R')
+    print('        T T T                         O O O O O O O O              R R              R R')
     print(Fore.GREEN + Style.BRIGHT + 'by emcruise' + STYLE.RESET_ALL)
     
